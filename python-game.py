fe8976749c06930caa221c131bc249d2692ddf6d
zainanz <zainan.zaher09@gmail.com>
2023-05-16 17:01:39 +0100

    Creating a board

--- python-game.py
+++ python-game.py
@@ -1,1 +1,15 @@
-print('Hello World!')
+board = [[' ', ' ', ' '],[ ' ',' ',' '], [' ',' ',' ']]
+is_Finished = False
+
+def createBoard(board):
+    print(
+        f"""
+            {board[0][0]} | {board[0][1]} | {board[0][2]}
+            ----------
+            {board[1][0]} | {board[1][1]} | {board[1][2]}
+            ----------
+            {board[2][0]} | {board[2][1]} | {board[2][2]}""")
+    
+
+
+createBoard(board)
